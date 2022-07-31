319cc44fd73aef9a9277a4c6a422865cc3321192
aimansae <saeedaimann@gmail.com>
2022-07-31 11:38:05 +0000

    Create name function to gree the player with data validation conditions

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+print('Welcome to the ultimate TIC TAC TOE Grame!\n')
+
+
+def player_name():
+    '''
+    Gets player's name accepting only alphabetical characters
+    '''
+
+    while True:
+        name = input("Please Enter your name: ").lower()
+
+        if name.isalpha():
+            print(
+                f'{name} Welcome to the game . \n Please Follow the instructions below: ')
+            break
+
+        else:
+            print(
+                f'{name} is Invalid username. Only name with letter accepted. Try again')
+
+
+player_name()
